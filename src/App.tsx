f38e9a525b9c7770a9462966c73f71d0550c8c1c
AAro <aaro.se7en@gmail.com>
2025-10-15 16:22:13 +0330

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { Provider } from "react-redux";
+
+import { store } from "./store/store";
+
+import Navbar from "./components/Navbar";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
+
+import { productData } from "./data/items";
+
 function App() {
   return (
-    <div>App</div>
+    <Provider store={store}>
+      <Navbar/>
+      <ProductList>
+        {productData.map((item) => (
+          <div key={item.id}>
+            <Product {...item} />
+          </div>
+        ))}
+      </ProductList>
+    </Provider>
   )
 }
 
---
+++ src/components/Product.tsx
@@ -0,0 +1,34 @@
+
+import { useCartDispatch } from "../store/hooks";
+
+import { addToCart } from "../store/cart-slice";
+
+type ProductProps = {
+    id: string;
+    title: string;
+    price: number;
+    image: string;
+}
+
+
+function Product({id, title, price, image} : ProductProps) {
+    const dispatch = useCartDispatch();
+
+    function handleAddToCart() {
+     dispatch(addToCart({ id, title, price }))
+    }
+   return (
+    <div className="product">
+        <img src={image} />
+        <div>
+            <h3>{title}</h3>
+            <p className="product-price">{price}</p>
+        </div>
+        <p className="product-actions">
+            <button onClick={handleAddToCart}>افزودن به سبد خرید</button>
+        </p>
+    </div>
+   )
+}
+
+export default Product;
